427c92f1d4754ebe9a20a9479b363cd166ccc23f
Ed Hillmann <edward.p.hillmann@boeing.com>
2025-07-01 13:30:35 +1000

    Script tweaking

--- install.sh
+++ install.sh
@@ -7,3 +7,4 @@ DUCKDB_EXTENSIONS=${DUCKDB_HOME}/extensions
 mkdir -p ${DUCKDB_EXTENSIONS}/v1.3.1/linux_amd64
 
 cp iceberg.duckdb_extension.gz ${DUCKDB_EXTENSIONS}/v1.3.1/linux_amd64
+gunzip ${DUCKDB_EXTENSIONS}/v1.3.1/linux_amd64/iceberg.duckdb_extension.gz
